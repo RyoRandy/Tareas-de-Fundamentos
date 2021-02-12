174f3f10e84ee1fafd9735772b142c14cbbf0bef
RandyMejia <202010307@itla.edu.do>
2021-02-12 18:42:18 -0400

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tareas_de_Fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Klk?");
+        }
+    }
+}
