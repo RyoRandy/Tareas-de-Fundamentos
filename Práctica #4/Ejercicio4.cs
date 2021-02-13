a4b0fe601b887563ac1d14303a358a29397874bd
RandyMejia <202010307@itla.edu.do>
2021-02-12 21:30:40 -0400

    Last Update - Práctica #4

---
+++ Práctica #4/Ejercicio4.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Tareas_de_Fundamentos
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            char letra;
+
+            for (letra='z'; letra>='a'; letra--) {
+                Console.WriteLine("{0} ", letra);
+            }
+        }
+    }
+}
