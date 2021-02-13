64ed9a2c89018930b7ca0684d2f24493651c35d1
RandyMejia <202010307@itla.edu.do>
2021-02-12 20:05:01 -0400

    Ejercicio #1 commit

---
+++ Ejercicio-#1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tareas_de_Fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, suma = 0;
+            do {
+                Console.WriteLine("Introduzca una cantidad de números positivos, ingrese el número 0 cuando quiera el resultado.");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"El numero es: {num}");
+                suma = suma + num;
+            }
+            while(num != 0 && num > 0);
+            Console.WriteLine($"La suma de los números da un total de: {suma}");
+        }
+    }
+}
