4df241de0075ea0bf1f009fee2eedcea6d2b8024
RandyMejia <202010307@itla.edu.do>
2021-02-12 21:01:10 -0400

    a

---
+++ Práctica #4/Ejercicio1.cs
@@ -0,0 +1,21 @@
+
+using System;
+
+namespace Tareas_de_Fundamentos
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            int num, suma = 0;
+            do {
+                Console.WriteLine("Introduzca una cantidad de números positivos, ingrese el número 0 cuando quiera el resultado.");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"El numero es: {num}");
+                suma = suma + num;
+            }
+            while(num != 0 && num > 0);
+            Console.WriteLine($"La suma de los números da un total de: {suma}");
+        }
+    }
+}
