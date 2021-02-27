d55cbdd374a46dd5e3efc8e0b4b2647eceff4402
RandyMejia <202010307@itla.edu.do>
2021-02-26 23:15:14 -0400

    Esto es un commit

---
+++ Practice #5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tareas_de_Fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Bitches");
+        }
+    }
+}
